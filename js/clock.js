6761e4fff78413471c83aa8b8f197d34702ca8b0
wook2124 <wook2124@naver.com>
2020-06-15 03:23:18 +0900

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,33 +1,34 @@
-const timer = document.querySelector('.js-clock')
-let overTime = true
+const timer = document.querySelector(".js-clock");
+let overTime = true;
 
 function setTime(hour) {
-    if (hour > 12 && overTime) {
-        hour -= 12
-    }
-    return hour
+  if (hour > 12 && overTime) {
+    hour -= 12;
+  }
+  return hour;
 }
 
 function getTime() {
-    const dateNow = new Date()
-    let hour = dateNow.getHours()
-    const hour24 = dateNow.getHours()
-    const minute = dateNow.getMinutes()
-    const seconds = dateNow.getSeconds()
+  const dateNow = new Date();
+  let hour = dateNow.getHours();
+  const hour24 = dateNow.getHours();
+  const minute = dateNow.getMinutes();
+  // const seconds = dateNow.getSeconds()
 
-    hour = setTime(hour)
+  hour = setTime(hour);
 
-    timer.innerHTML =
-        `
-            ${hour24 < 12 ? 'AM' : 'PM'}
+  timer.innerHTML = `
+            ${hour24 < 12 ? "AM" : "PM"}
             ${hour < 10 ? `0${hour}` : hour} :
             ${minute < 10 ? `0${minute}` : minute}
-        `
+        `;
+
+  // greetingMessage(hour); // greeting.js(for different greet message)
 }
 
 function init() {
-    getTime()
-    setInterval(getTime, 1000)
+  getTime();
+  setInterval(getTime, 1000);
 }
 
-init()
+init();
