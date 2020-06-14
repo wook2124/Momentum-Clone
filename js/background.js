5224b5a521f8a53932a00e604a0ae6e547197495
wook2124 <wook2124@naver.com>
2020-06-15 02:39:00 +0900

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,20 @@
+const body = document.querySelector("body")
+const IMAGE_COUNT = 3
+
+function getRandom() {
+    return Math.floor(Math.random() * IMAGE_COUNT)
+}
+
+function setImage(ran) {
+    const img = new Image()
+    img.src = `wallpaper/wallpaper${ran + 1}.jpg`
+    img.classList.add('bg-img')
+    body.prepend(img)
+}
+
+function init() {
+    const ran = getRandom()
+    setImage(ran)
+}
+
+init()
